b3722b162fee348b957a0c4c6541c3c42b6f939d
LMartinezEXEX <lautaro.martinezexex@gmail.com>
2022-12-12 12:53:39 -0300

    Updated model and tokenizer HugingFace loading with Auto... clases

--- modules/module_languageModel.py
+++ modules/module_languageModel.py
@@ -1,10 +1,10 @@
-from transformers import BertForMaskedLM, BertTokenizer
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 
 class LanguageModel:
     def __init__(self, model_name):
         print("Download language model...")
-        self.__tokenizer = BertTokenizer.from_pretrained(model_name)
-        self.__model = BertForMaskedLM.from_pretrained(model_name, return_dict=True)
+        self.__tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.__model = AutoModelForMaskedLM.from_pretrained(model_name)
     
     def initTokenizer(self):
         return self.__tokenizer
