ae9167868f51f85d79a69b6b2946a91e4d52daf6
Maina, Hernán J <hernan.maina@mi.unc.edu.ar>
2023-01-17 19:13:42 -0300

    Added ./setup.sh script to automatically download all data files. Assigning versions to requirement file libraries

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+# HOW TO USE:
+#   $ ./setup.sh
+
+WORKDIR=$(pwd)
+EDIA_DATA_DIR="$WORKDIR/data_test"
+
+echo "* Creating app/data/ diretory ..."
+mkdir $EDIA_DATA_DIR
+
+echo "* Download data files ..."
+ID="1uI6HsBw1XWVvTEIs9goSpUVfeVJe-zEP"
+wget -q --show-progress "https://drive.google.com/uc?export=download&id=$ID&export=download&confirm=yes" -O "$EDIA_DATA_DIR/mini_vocab_v6.zip"
+ID="1T_pLFkUucP-NtPRCsO7RkOuhMqGi41pe"
+wget -q --show-progress "https://drive.google.com/uc?export=download&id=$ID&export=download&confirm=yes" -O "$EDIA_DATA_DIR/full_vocab_v6.zip"
+ID="1EN0pp1RKyRwi072QhVWJaDO8KlcFZo46"
+wget -q --show-progress "https://drive.google.com/uc?export=download&id=$ID&export=download&confirm=yes" -O "$EDIA_DATA_DIR/100k_en_embedding.vec"
+ID="1YwjyiDN0w54P55-y3SKogk7Zcd-WQ-eQ"
+wget -q --show-progress "https://drive.google.com/uc?export=download&id=$ID&export=download&confirm=yes" -O "$EDIA_DATA_DIR/100k_es_embedding.vec"
